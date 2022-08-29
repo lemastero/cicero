c68e1ae21adb24fb9434c630a91922c9106f0291
Robin Stumm <robin.stumm@iohk.io>
2022-08-29 13:30:37 +0200

    run CI for every branch

--- tullia/actions.nix
+++ tullia/actions.nix
@@ -2,16 +2,9 @@
   "cicero/ci" = {
     task = "build";
     io = ''
-      let github = {
-        #input: "GitHub event"
-        #repo: "input-output-hk/cicero"
-      }
-
-      #lib.merge
-      #ios: [
-        #lib.io.github_push & github,
-        #lib.io.github_pr   & github,
-      ]
+      #lib.io.github_push
+      #repo: "input-output-hk/cicero"
+      #default_branch: false
     '';
   };
 }
